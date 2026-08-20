e53a0cbff4e731506cbec73c0a6937f913deb34b
Maria Madalena <mariamada496@gmail.com>
2026-08-20 02:30:03 +0000

    questão 6

---
+++ lista04/questao6.py
@@ -0,0 +1,13 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+class ListaEncadeada:
+    def __init__(self):
+        self.cabeca = None
+
+def particao(lista, k):
+    # Percorre l1 e separa em duas frentes
+   
+# Complexidade: O(n)
